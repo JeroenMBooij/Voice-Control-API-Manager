cf4a3ff66be45d9b99be258c92241b943919be93
JeroenMBooij <49719495+JeroenMBooij@users.noreply.github.com>
2021-05-24 02:20:24 +0200

    try again

--- src/api/build/config/index.ts
+++ src/api/build/config/index.ts
@@ -4,4 +4,4 @@ export default {
     PRIVATE_KEY: process.env.PRIVATE_KEY || fs.readFileSync('jwtRS256.key'),
     PUBLIC_KEY: process.env.PUBLIC_KEY || fs.readFileSync('jwtRS256.key.pub'),
     port: parseInt(process.env.PORT || '3000', 10)
-  }; 
+  };  
